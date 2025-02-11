8e03cba46bb9f5b5df6abfd6b7379be42e978920
Thomas Ropert <thomas.ropert@yahoo.fr>
2025-02-11 20:54:23 +0100

    Ajout page covoiturage

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,7 +1,9 @@
 import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/", "Covoiturage", "/pages/covoiturage.html"),
+];
 
     
 //Le titre s'affiche comme ceci : Route.titre - websitename

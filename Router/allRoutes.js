3fcbb9d50f238bed3857b237d953619e39f0ad1c
Thomas Ropert <thomas.ropert@yahoo.fr>
2025-04-03 19:14:36 +0200

    Ajout styles

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,9 +3,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
-];
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/creerTrajet", "Créer un trajet", "/pages/creerTrajet.html"),
+];  
 
     
 //Le titre s'affiche comme ceci : Route.titre - websitename
